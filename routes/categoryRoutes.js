1240e61ac6afc660a42e19b20741e3afb8396e0f
khadija Elhardi <elhardykhadija@gmail.com>
2023-02-18 23:23:49 +0100

    categoryRoutes

--- routes/categoryRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const catalogueController = require("../controllers/catalogue.controller");
-
-router.get("/", catalogueController.getCategories);
-router.get("/:id", catalogueController.ge);
-
-module.exports = router;
